42652949c08b0c4a6323f007abaa8ba83cba38d2
axeliono <69375446+axeliono@users.noreply.github.com>
2021-02-04 18:10:57 -0600

    starting backend

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <Navbar bg="light" variant="light" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand>ProMart</Navbar.Brand>
+            <Navbar.Brand>Shopper Pro</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
